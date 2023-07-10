9c6b83faaac24a178a08093ebae3aa3086a71f99
TimothyXia9 <77732791+TimothyXia9@users.noreply.github.com>
2023-07-10 17:52:46 +0800

    can add comment

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from . import views
 
-app_name = 'books'
+app_name = "books"
 
 urlpatterns = [
-    path('detail/<int:book_id>/<int:page>', views.DetailView.as_view(), name='detail'),
-    path('category/<int:category_id>/<int:page>', views.CategoryView.as_view(), name='category'),
+    path("detail/<int:book_id>/<int:page>", views.DetailView.as_view(), name="detail"),
+    path(
+        "category/<int:category_id>/<int:page>",
+        views.CategoryView.as_view(),
+        name="category",
+    ),
+    path(
+        "detail/<int:book_id>/<int:page>/submit_comment",
+        views.CommentSubmitView.as_view(),
+        name="comment_submit",
+    ),
 ]
